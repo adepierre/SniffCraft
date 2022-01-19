dd8ba6347733cb4e1380edb6787cc2d1ea29f58a
adepierre <24371370+adepierre@users.noreply.github.com>
2022-01-19 19:22:32 +0100

    Fix quotations in cmake files

--- cmake/botcraft.cmake
+++ cmake/botcraft.cmake
@@ -1,8 +1,8 @@
 #Download botcraft library
 
-file(GLOB RESULT ${CMAKE_SOURCE_DIR}/3rdparty/botcraft/protocolCraft)
+file(GLOB RESULT "${CMAKE_CURRENT_SOURCE_DIR}/3rdparty/botcraft/protocolCraft")
 list(LENGTH RESULT RES_LEN)
 if(RES_LEN EQUAL 0)
     message(STATUS "Botcraft not found, cloning it...")
-    execute_process(COMMAND git submodule update --init -- 3rdparty/botcraft WORKING_DIRECTORY ${CMAKE_SOURCE_DIR})
+    execute_process(COMMAND git submodule update --init -- 3rdparty/botcraft WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}")
 endif()
