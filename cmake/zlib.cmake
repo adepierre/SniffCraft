be568374129df1389459dd5c71f1ee9e9d4d06e7
adepierre <24371370+adepierre@users.noreply.github.com>
2025-06-02 22:13:11 +0200

    Update zlib version

--- cmake/zlib.cmake
+++ cmake/zlib.cmake
@@ -7,25 +7,28 @@ endif(NOT SNIFFCRAFT_FORCE_LOCAL_ZLIB)
 
 # If not found, build from sources
 if(NOT TARGET ZLIB::ZLIB)
-    message(STATUS "Can't find Zlib, cloning and building it from sources")
-
-    file(GLOB RESULT "${CMAKE_CURRENT_SOURCE_DIR}/3rdparty/zlib/examples")
-    list(LENGTH RESULT RES_LEN)
-    if(RES_LEN EQUAL 0)
-        message(STATUS "Zlib not found, cloning it...")
-        execute_process(COMMAND git submodule update --init -- 3rdparty/zlib WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}")
-    endif()
-
-    set(ZLIB_SRC_PATH "${CMAKE_CURRENT_SOURCE_DIR}/3rdparty/zlib")
+    set(ZLIB_SRC_PATH "${CMAKE_CURRENT_SOURCE_DIR}/3rdparty/zlib/")
     set(ZLIB_BUILD_PATH "${CMAKE_CURRENT_BINARY_DIR}/3rdparty/zlib")
 
-    file(MAKE_DIRECTORY "${ZLIB_BUILD_PATH}")
+    file(GLOB RESULT "${ZLIB_BUILD_PATH}/install")
+    list(LENGTH RESULT RES_LEN)
+    if(RES_LEN EQUAL 0)
+        message(STATUS "Can't find Zlib, cloning and building it from sources")
 
-    execute_process(
-        COMMAND "${CMAKE_COMMAND}" "${ZLIB_SRC_PATH}" "-G" "${CMAKE_GENERATOR}" "-DCMAKE_BUILD_TYPE=Release" "-A" "${CMAKE_GENERATOR_PLATFORM}" "-DCMAKE_INSTALL_PREFIX=install"  "-DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded" "-DCMAKE_POLICY_DEFAULT_CMP0091=NEW"
-        WORKING_DIRECTORY "${ZLIB_BUILD_PATH}")
+        file(GLOB RESULT "${ZLIB_SRC_PATH}")
+        list(LENGTH RESULT RES_LEN)
+        if(RES_LEN EQUAL 0)
+            execute_process(COMMAND git submodule update --init -- 3rdparty/zlib WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}")
+        endif()
 
-    execute_process(COMMAND "${CMAKE_COMMAND}" "--build" "." "--target" "install" "--config" "Release" "--parallel" "2" WORKING_DIRECTORY "${ZLIB_BUILD_PATH}")
+        file(MAKE_DIRECTORY "${ZLIB_BUILD_PATH}")
+
+        execute_process(
+            COMMAND "${CMAKE_COMMAND}" "${ZLIB_SRC_PATH}" "-G" "${CMAKE_GENERATOR}" "-DCMAKE_BUILD_TYPE=Release" "-A" "${CMAKE_GENERATOR_PLATFORM}" "-DCMAKE_MAKE_PROGRAM=${CMAKE_MAKE_PROGRAM}" "-DCMAKE_INSTALL_PREFIX=install"  "-DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded" "-DCMAKE_POLICY_DEFAULT_CMP0091=NEW" "-DZLIB_BUILD_SHARED=OFF"
+            WORKING_DIRECTORY "${ZLIB_BUILD_PATH}")
+
+        execute_process(COMMAND "${CMAKE_COMMAND}" "--build" "." "--target" "install" "--config" "Release" "--parallel" "2" WORKING_DIRECTORY "${ZLIB_BUILD_PATH}")
+    endif()
 
     # Find the freshly built library
 
@@ -33,16 +36,18 @@ if(NOT TARGET ZLIB::ZLIB)
     # but before, we need to do it in a more manual way
     if(${CMAKE_VERSION} VERSION_LESS "3.24.0")
         set(_CMAKE_FIND_LIBRARY_SUFFIXES ${CMAKE_FIND_LIBRARY_SUFFIXES})
-        set(CMAKE_FIND_LIBRARY_SUFFIXES "static.lib" ".a")
+        set(CMAKE_FIND_LIBRARY_SUFFIXES "static.lib" "s.lib" ".a")
     else()
         set(ZLIB_USE_STATIC_LIBS "ON")
     endif()
 
-    set(ZLIB_ROOT "${ZLIB_BUILD_PATH}/install")
-    find_package(ZLIB QUIET)
+    find_package(ZLIB QUIET NO_DEFAULT_PATH PATHS "${ZLIB_BUILD_PATH}/install")
+
+    # We link to ZLIB::ZLIB so we need an alias if we're using the static local version
+    add_library(ZLIB::ZLIB ALIAS ZLIB::ZLIBSTATIC)
 
     if(${CMAKE_VERSION} VERSION_LESS "3.24.0")
-        set(CMAKE_FIND_LIBRARY_SUFFIXES ${_CMAKE_FIND_LIBRARY_SUFFIXES}) 
+        set(CMAKE_FIND_LIBRARY_SUFFIXES ${_CMAKE_FIND_LIBRARY_SUFFIXES})
         unset(_CMAKE_FIND_LIBRARY_SUFFIXES)
     endif()
 endif()
